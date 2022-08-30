03c04400c0faa131f0cdd26032fa49ffae60e4eb
micha <micha030201@gmail.com>
2022-08-31 00:52:04 +1000

    allow creating multiband geotiff from multiple files

--- to_geotiff/main.py
+++ to_geotiff/main.py
@@ -1,8 +1,19 @@
-import proformat.readpro as pro
-from pyproj import Transformer
+import argparse
+from os.path import commonprefix
+
 import numpy as np
 import rasterio
-import sys
+from pyproj import Transformer
+
+import proformat.readpro as pro
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('in_', metavar='in', nargs='+',
+                    help='Один или несколько входных файлов в формате .pro.')
+parser.add_argument('out',
+                    help='Имя выходного файла в формате GeoTIFF.')
+args = parser.parse_args()
 
 
 # Функция для чтения формата данных .pro и последующего создания
@@ -64,25 +75,81 @@ def affine_matrix(pro_passport):
 
 
 # Функция для создания GeoTIFF файла из файла с форматом .pro.
-def create_geotiff(file_name):
-    pro_file_name = sys.argv[1] # Считываем имя файла в формате .pro с консоли.
-    pro_file = open(pro_file_name, 'rb')
+def create_geotiff():
+    file_name = args.out
+    if not (file_name.endswith('.tif') or file_name.endswith('.tiff')):
+        file_name += '.tif'
 
-    pro_passport = parse_pro_passport(pro_file)
-    data = parse_pro_data(pro_file, pro_passport)
-    affine_mat = affine_matrix(pro_passport)
+    # Нам нужно знать, каких размеров нужно создать geotiff, поэтому
+    # паспорт первого входного файла дополнительно считываем заранее.
+    with open(args.in_[0], 'rb') as pro_file:
+        pro_passport_first = parse_pro_passport(pro_file)
 
-    file_name += '.tif'
+    # Для красоты уберём общий префикс и расширение из названия слоя.
+    if len(args.in_) > 1:
+        band_names = [n.split('.') for n in args.in_]
+        p = commonprefix(band_names)
+        band_names = [
+            '.'.join(n[len(p):-1] if n[-1] == 'pro' else n[len(p):])
+            for n in band_names
+        ]
+
+    # TODO скопировать больше (все?) данных из паспорта в теги geotiff
+
     with rasterio.Env(GDAL_TIFF_INTERNAL_MASK=True):
         with rasterio.open(
                 file_name, 'w',
                 driver='GTiff',
-                height=pro_passport['lines'], width=pro_passport['pixels'],
-                count=1, dtype=data.dtype,
-                crs='EPSG:3395', transform=affine_mat,
+                height=pro_passport_first['lines'],
+                width=pro_passport_first['pixels'],
+                count=len(args.in_), dtype='float64',
+                crs='EPSG:3395', transform=affine_matrix(pro_passport_first),
                 compress='deflate', num_threads='all_cpus'
                 ) as geotiff:
-            geotiff.write(data, 1)
-            geotiff.write_mask(data > 0)
 
-create_geotiff(sys.argv[2])
+            mask = np.full(
+                (pro_passport_first['lines'], pro_passport_first['pixels']),
+                True
+            )
+
+            for i, (pro_file_name, band_name) in \
+                    enumerate(zip(args.in_, band_names), start=1):
+                if not pro_file_name.endswith('.pro'):
+                    print(pro_file_name + ' не имеет расширение .pro')
+
+                with open(pro_file_name, 'rb') as pro_file:
+                    pro_passport = parse_pro_passport(pro_file)
+
+                    diff = '\n'.join(
+                        '{k}: {v1} != {v2}'.format(
+                            k=k,
+                            v1=pro_passport[k],
+                            v2=pro_passport_first[k]
+                        )
+                        for k in pro_passport.keys()
+                        if pro_passport[k] != pro_passport_first[k]
+                        and k not in {'coeff_a', 'coeff_b', 'max_pixel'}
+                        # Я не знаю, почему резервные поля могут
+                        # отличаться, но это так.  На моих данных
+                        # отличался только norad_reserve, но я на всякий
+                        # случай включил все резервные поля.
+                        and not k.endswith('_reserve')
+                    )
+                    if diff:
+                        raise ValueError(
+                            'Паспорта входных файлов не совпадают:\n' + diff
+                        )
+
+                    data = parse_pro_data(pro_file, pro_passport)
+
+                # rasterio doesn't have the best API ok
+                geotiff.write(data, i)
+                geotiff.set_band_description(i, band_name)
+
+                mask &= data > 0
+
+            geotiff.write_mask(mask)
+
+
+if __name__ == '__main__':
+    create_geotiff()
